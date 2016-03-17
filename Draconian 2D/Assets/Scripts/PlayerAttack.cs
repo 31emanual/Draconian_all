8549e60d2621a8210a13ffa1baedfd2faec058cf
31emanual <mapurefoy@gmail.com>
2016-03-17 11:35:52 -0400

    fixes dash attack

--- Draconian 2D/Assets/Scripts/PlayerAttack.cs
+++ Draconian 2D/Assets/Scripts/PlayerAttack.cs
@@ -1,93 +1,110 @@
-﻿using UnityEngine;
+﻿﻿using UnityEngine;
 using System.Collections;
 using UnityStandardAssets._2D;
 
-public class PlayerAttack : MonoBehaviour 
+public class PlayerAttack : MonoBehaviour
 {
-	//private bool chargingDash;
-	//private bool dashing;
-	public float chargeStart = 100f;
-	public float chargeCounter;
-	public Vector2 mousePos;
-	private Rigidbody2D m_Rigidbody2D;
-	private Vector2 dashVector;
-	public float dashForce = 100f;
+    //private bool chargingDash;
+    //private bool dashing;
+    public float chargeStart = 100f;
+    public float chargeCounter;
+    public Vector2 mousePos;
+    private Rigidbody2D m_Rigidbody2D;
+    private Vector2 dashVector;
+    public float initialDashForce = 50f;
+    public float dashIncrement = 1f;
+    public float dashForce;
+    public float maxDashForce = 100f;
     //public float vectorSaveTime = .4f;
     public float dashDuration;
     public float dashStop = 1f;
-    public float upTimer;
+    // public float upTimer;
     private PlatformerCharacter2D character;
 
 
+    public bool dashing
+    {
+        get { return dashDuration < dashStop; }
+    }
 
 
     // Use this for initialization
-    void Start () 
-	{
-        character = (PlatformerCharacter2D)GameObject.Find("CharacterRobotBoy").GetComponent(typeof(PlatformerCharacter2D));
-        Debug.Log(character.AirControl);
+    void Start()
+    {
+        character = GetComponent<PlatformerCharacter2D>();
 
         m_Rigidbody2D = GetComponent<Rigidbody2D>();
-		//chargingDash = false;
-		//dashing = false;
-		chargeCounter = 0f;
+        //chargingDash = false;
+        //dashing = false;
+        chargeCounter = 0f;
         dashDuration = dashStop;
-        upTimer = 0f;
+        //upTimer = 0f;
+        dashForce = initialDashForce;
     }
-	
-	// Update is called once per frame
-	void Update () 
-	{
-	 
-	}
-	private void FixedUpdate()
-	{
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void FixedUpdate()
+    {
         dashDuration += Time.deltaTime;
 
         mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-		Debug.DrawLine (transform.position, mousePos);
-		Debug.Log (chargeCounter);
+        Debug.DrawLine(transform.position, mousePos);
 
-        Debug.Log(upTimer);
+        // Debug.Log(upTimer);
 
-        if (Input.GetButton ("Fire1")) 
-		{
-		
-			chargeCounter++;
+        if (Input.GetButton("Fire1"))
+        {
 
-		
-			//if (chargeCounter >= chargeStart)
-				//dashing = true;
-				//StartCoroutine(DashChargeRoutine());
-			 
-		}
+            chargeCounter++;
+            if (chargeCounter >= chargeStart)
+
+                dashForce += (dashIncrement * Time.deltaTime);
+
+            if (dashForce > maxDashForce)
+
+                dashForce = maxDashForce;
 
 
-		if (Input.GetButtonUp ("Fire1")) 
-		{				
-			if (chargeCounter >= chargeStart)
-			{
+            //if (chargeCounter >= chargeStart)
+            //dashing = true;
+            //StartCoroutine(DashChargeRoutine());
+
+        }
+
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            if (chargeCounter >= chargeStart)
+            {
                 dashDuration = 0f;
                 // dashDuration += (dashDuration * Time.deltaTime);
-                dashVector = new Vector2(mousePos.x - transform.position.x, mousePos.y - transform.position.y );
-				m_Rigidbody2D.AddForce(dashVector *dashForce, ForceMode2D.Impulse);
+                dashVector = new Vector2(mousePos.x - transform.position.x, mousePos.y - transform.position.y);
+                m_Rigidbody2D.AddForce(dashVector * dashForce, ForceMode2D.Impulse);
+                dashForce = initialDashForce;
                 //dashing = true;
                 //StartCoroutine( DashRoutine());
                 //dashDuration = 0f;
-                upTimer += (upTimer * Time.deltaTime);
+                //upTimer += (upTimer * Time.deltaTime);
 
-               
+
 
             }
-           
-            chargeCounter = 0f;			
-				
-			   
-		}
+
+            chargeCounter = 0f;
+
+
+        }
         if (dashDuration < dashStop)
+        {
             character.AirControl = false;
-        else
+        }
+        else {
             character.AirControl = true;
+        }
 
 
 
